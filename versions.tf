3eb27d3492aa064761687d1246eb49d4bb5797d4
Vedant Pareek <dunefro@gmail.com>
2024-02-27 13:14:03 +0530

    Updated GCP module

--- versions.tf
+++ versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "5.10.0"
+      version = "5.18.0"
     }
     google = {
       source  = "hashicorp/google"
-      version = "5.10.0"
+      version = "5.18.0"
     }
   }
 }
