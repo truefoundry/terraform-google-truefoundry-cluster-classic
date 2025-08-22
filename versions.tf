cd10ab3af4d71eba3959967f2278f221a43cddc0
Vedant Pareek <dunefro@gmail.com>
2025-08-22 13:43:54 +0530

    terraform fmt

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "6.47"
+      version = "6.47" #6.48 bug - https://github.com/issues/created?issue=hashicorp%7Cterraform-provider-google%7C24069
     }
     google = {
       source  = "hashicorp/google"
